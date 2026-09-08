0bda4b702f479062afdd5418ef5b4dae12c9dc5a
Aryan Sahu <135861400+ary-beedee@users.noreply.github.com>
2026-09-08 09:28:26 +0000

    dummy api set up

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,7 +10,7 @@ function Product() {
   const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/products?limit=194`)
+    fetch("https://dummyjson.com/products?limit=194")
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, [id]);
